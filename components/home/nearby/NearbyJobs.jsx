b9aad1e4767a5f2195de9d4d5c83278d21df14bd
Cardiaca <owendavies802@gmail.com>
2024-01-26 21:08:47 -0800

    tabs and linter settings

--- components/home/nearby/NearbyJobs.jsx
+++ components/home/nearby/NearbyJobs.jsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native';
+import React, { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { useRouter } from 'expo-router';
 
 import styles from './nearbyjobs.style';
@@ -7,42 +7,42 @@ import NearbyJobCard from '../../common/cards/nearby/NearbyJobCard';
 import useFetch from '../../../hook/useFetch';
 
 const NearbyJobs = () => {
-  const router = useRouter();
-  const { data, isLoading, error } = useFetch('search', {
-    query: 'Ping Pong',
-    num_pages: 1
-  })
+	const router = useRouter();
+	const { data, isLoading, error } = useFetch('search', {
+		query: 'Ping Pong',
+		num_pages: 1
+	});
 
-  const handleCardPress = () => {
-    alert('Hi');
-  }
+	const handleCardPress = () => {
+		alert('Hi');
+	};
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerTitle}>Nearby jobs</Text>
-        <TouchableOpacity>
-          <Text style={styles.headerBtn}>Show all</Text>
-        </TouchableOpacity>
-      </View>
+	return (
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Text style={styles.headerTitle}>Nearby jobs</Text>
+				<TouchableOpacity>
+					<Text style={styles.headerBtn}>Show all</Text>
+				</TouchableOpacity>
+			</View>
 
-      <View style={styles.cardsContainer}>
-        {isLoading ? (
-          <ActivityIndicator size='large' colors={COLORS.primary} />
-        ) : error ? (
-          <Text>Something went wrong</Text>
-        ) : (
-          data?.map((job, index) => (
-            <NearbyJobCard
-              job={job}
-              key={job.job_id ? `nearby-job-${job.job_id}` : `nearby-job-${index}`}
-              handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
-            />
-          ))
-        )}
-      </View>
-    </View>
-  )
-}
+			<View style={styles.cardsContainer}>
+				{isLoading ? (
+					<ActivityIndicator size="large" colors={COLORS.primary} />
+				) : error ? (
+					<Text>Something went wrong</Text>
+				) : (
+					data?.map((job, index) => (
+						<NearbyJobCard
+							job={job}
+							key={job.job_id ? `nearby-job-${job.job_id}` : `nearby-job-${index}`}
+							handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
+						/>
+					))
+				)}
+			</View>
+		</View>
+	);
+};
 
 export default NearbyJobs;
